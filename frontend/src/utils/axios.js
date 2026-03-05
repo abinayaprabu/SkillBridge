168621038f5643b7b6f4e097138214276a7e4eb8
abinayaprabu <abinaya2310921@ssn.edu.in>
2026-03-05 09:47:51 +0530

    Remove withCredentials to fix CORS

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: true,
 });
 
 instance.interceptors.request.use((config) => {
