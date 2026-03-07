f4caf5ccc4dce240b5968e7dd35ed558918938f3
abinayaprabu <abinaya2310921@ssn.edu.in>
2026-03-07 11:56:16 +0530

    fix smtp connection

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,18 +1,25 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, text) => {
-
   try {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
       secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false
+      }
     });
+
+    // check connection
+    await transporter.verify();
+    console.log("SMTP server ready");
 
     await transporter.sendMail({
       from: `"SkillBridge" <${process.env.EMAIL_USER}>`,
@@ -24,9 +31,6 @@ export const sendEmail = async (to, subject, text) => {
     console.log("Email sent successfully");
 
   } catch (error) {
-
-    console.error("Email Error:", error.message);
-
+    console.error("Email Error:", error);
   }
-
 };
