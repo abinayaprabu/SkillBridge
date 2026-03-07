ffa32dead01336e8cc2d12f63c8b5b7abf31493b
abinayaprabu <abinaya2310921@ssn.edu.in>
2026-03-07 11:34:00 +0530

    fix email transporter

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,14 +5,13 @@ export const sendEmail = async (to, subject, text) => {
   try {
 
     const transporter = nodemailer.createTransport({
-
-      service: "gmail",
-
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-
     });
 
     await transporter.sendMail({
